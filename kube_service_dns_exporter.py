19d70b38ee90ab7d4c03d75189ec106a195b81ca
Ivan Li <email2liyang@gmail.com>
2019-10-04 19:50:56 +0800

    could detect the cluster ip on creation and deletion

--- kube_service_dns_exporter.py
+++ kube_service_dns_exporter.py
@@ -1,14 +1,15 @@
-import os
-
 import kopf
 import pykube
 
 
 @kopf.on.create('', 'v1', 'services')
-def create_fn(meta, body, spec, namespace, logger, **kwargs):
+def create_fn(spec, logger, **kwargs):
     api = pykube.HTTPClient(pykube.KubeConfig.from_file())
     print(f"creating service with {spec}")
     print(f"cluster ip is {spec['clusterIP']}")
-    return {'message': 'hello world'}
 
 
+@kopf.on.delete('', 'v1', 'services')
+def delete_fn(spec,logger, **kwargs):
+    print(f"deleting service with {spec}")
+    print(f"cluster ip is {spec['clusterIP']}")
